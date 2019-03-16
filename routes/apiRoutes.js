fbbcf757d1b9e579f4bfff325107853c358b1548
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-03-16 10:09:09 -0500

    Hate myself

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/beerlist", function(req, res) {
+  app.post("/api/posts", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
